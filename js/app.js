d3158e8fcf8ecf939062eac713914cc2438c4a8f
Zé Cipriano <ze@zecipriano.com>
2016-11-28 17:48:48 +0000

    Get movie posters

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 var app = new Vue({
     el: '#app',
+
     data: {
-        ratings: null
+        ratings: null,
+        posters: {}
     },
+
+    watch: {
+        ratings: function () {
+            this.populatePosters();
+        }
+    },
+
     methods: {
-        onFileChange(e) {
+        onFileChange: function(e) {
             var files = e.target.files || e.dataTransfer.files;
 
             Papa.parse(files[0], {
@@ -15,6 +24,30 @@ var app = new Vue({
                     this.ratings = results.data;
                 }.bind(this)
             });
-        }
+        },
+
+        populatePosters: function() {
+            var arrayLength = this.ratings.length;
+
+            for (var i = 0; i < arrayLength; i++) {
+
+                if(this.ratings[i]["Poster"] == null) {
+                    var imdbID = this.ratings[i]["const"];
+
+                    var config = {
+                        paramsSerializer: { arrayIndex : i }
+                    }
+
+                    axios.get('http://www.omdbapi.com/?i=' + imdbID, config)
+                        .then(function (response) {
+                            Vue.set(this.posters, response.data['imdbID'], response.data['Poster']);
+                        }.bind(this))
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                }
+            }
+        },
     }
+
 })
