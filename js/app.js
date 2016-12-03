2e27191f5a2a19a6a56054552aca50f9b4a377eb
Zé Cipriano <ze@zecipriano.com>
2016-12-03 14:16:03 +0000

    Show only the best movies of each year

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ var app = new Vue({
 
     data: {
         ratings: null,
-        posters: {}
+        posters: {},
+        bestPerYear: {}
     },
 
     watch: {
         ratings: function () {
             this.populatePosters();
+            this.populateBestPerYear();
         }
     },
 
@@ -40,6 +42,7 @@ var app = new Vue({
 
                     axios.get('https://www.omdbapi.com/?i=' + imdbID, config)
                         .then(function (response) {
+                            console.log(response);
                             Vue.set(this.posters, response.data['imdbID'], response.data['Poster']);
                         }.bind(this))
                         .catch(function (error) {
@@ -48,6 +51,29 @@ var app = new Vue({
                 }
             }
         },
+
+        populateBestPerYear: function() {
+            var arrayLength = this.ratings.length;
+
+            for (var i = 0; i < arrayLength; i++) {
+                var movie = this.ratings[i];
+
+                if(!this.bestPerYear[movie["Year"]]) {
+
+                    Vue.set(this.bestPerYear, movie["Year"], { bestRating: movie["You rated"], movies: [movie]});
+
+                } else if (this.bestPerYear[movie["Year"]].bestRating < movie["You rated"]) {
+
+                    Vue.set(this.bestPerYear[movie["Year"]], "bestRating", movie["You rated"]);
+                    Vue.set(this.bestPerYear[movie["Year"]], "movies", [movie]);
+
+                } else if (this.bestPerYear[movie["Year"]].bestRating === movie["You rated"]) {
+
+                    this.bestPerYear[movie["Year"]].movies.push(movie);
+
+                }
+            }
+        }
     }
 
 })
