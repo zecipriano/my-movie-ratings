00293d2adedefb2751f1d2a0d6085c89a067da16
Zé Cipriano <ze@zecipriano.com>
2017-07-14 16:52:12 +0100

    Remove OMDb API call

--- js/app.js
+++ js/app.js
@@ -3,64 +3,41 @@ var app = new Vue({
 
     data: {
         ratings: null,
-        posters: {},
         bestPerYear: {}
     },
 
     watch: {
         ratings: function () {
-            this.populatePosters();
             this.populateBestPerYear();
         }
     },
 
     methods: {
-        onFileChange: function(e) {
+        onFileChange: function (e) {
             var files = e.target.files || e.dataTransfer.files;
 
             Papa.parse(files[0], {
                 header: true,
                 dynamicTyping: true,
                 skipEmptyLines: true,
-                complete: function(results) {
+                complete: function (results) {
                     this.ratings = results.data;
                 }.bind(this)
             });
         },
 
-        populatePosters: function() {
-            var arrayLength = this.ratings.length;
-
-            for (var i = 0; i < arrayLength; i++) {
-
-                if(this.ratings[i]["Poster"] == null) {
-                    var imdbID = this.ratings[i]["const"];
-
-                    var config = {
-                        paramsSerializer: { arrayIndex : i }
-                    }
-
-                    axios.get('https://www.omdbapi.com/?i=' + imdbID, config)
-                        .then(function (response) {
-                            console.log(response);
-                            Vue.set(this.posters, response.data['imdbID'], response.data['Poster']);
-                        }.bind(this))
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                }
-            }
-        },
-
-        populateBestPerYear: function() {
+        populateBestPerYear: function () {
             var arrayLength = this.ratings.length;
 
             for (var i = 0; i < arrayLength; i++) {
                 var movie = this.ratings[i];
 
-                if(!this.bestPerYear[movie["Year"]]) {
+                if (!this.bestPerYear[movie["Year"]]) {
 
-                    Vue.set(this.bestPerYear, movie["Year"], { bestRating: movie["You rated"], movies: [movie]});
+                    Vue.set(this.bestPerYear, movie["Year"], {
+                        bestRating: movie["You rated"],
+                        movies: [movie]
+                    });
 
                 } else if (this.bestPerYear[movie["Year"]].bestRating < movie["You rated"]) {
 
