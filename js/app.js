ee3009bf8e5303ce948897c070e9c63cc88c67c2
Zé Cipriano <ze@zecipriano.com>
2017-07-21 11:00:36 +0100

    Put bestPerYear component in it own file

--- js/app.js
+++ js/app.js
@@ -8,60 +8,6 @@ let store = {
     },
 };
 
-const bestPerYear = {
-    data: function () {
-        return {
-            sharedState: store.state
-        }
-    },
-
-    template: `
-        <div class="ratings-list">
-            <div class="year" v-for="year in Object.keys(bestPerYear).sort().reverse()">
-                <h2 class="year__title">{{ year }}</h2>
-                <div class="movie" v-for="(movie, index) in bestPerYear[year].movies">
-                    <h3 class="movie__title">
-                        <a :href='movie["URL"]' target="_blank">
-                            {{ movie["Title"] }}
-                        </a>
-                    </h3>
-                    <p class="movie__ratings">
-                        <span class="movie__user-rating"><i class="fa fa-user" aria-hidden="true"></i> {{ movie["You rated"] }}</span>
-                        <span class="movie__all-users-rating"><i class="fa fa-users" aria-hidden="true"></i> {{ movie["IMDb Rating"] }}</span>
-                    </p>
-                </div>
-            </div>
-        </div>       
-    `,
-
-    computed: {
-        bestPerYear: function () {
-            let arrayLength = this.sharedState.ratings.length;
-            let bestPerYear = {};
-
-            for (let i = 0; i < arrayLength; i++) {
-                let movie = this.sharedState.ratings[i];
-                let year = movie["Year"];
-                let rating = movie["You rated"];
-
-                if (!bestPerYear[year]) {
-                    bestPerYear[year] = {
-                        bestRating: rating,
-                        movies: [movie]
-                    }
-                } else if (bestPerYear[year].bestRating < rating) {
-                    bestPerYear[year].bestRating = rating;
-                    bestPerYear[year].movies = [movie];
-                } else if (bestPerYear[year].bestRating === rating) {
-                    bestPerYear[year].movies.push(movie);
-                }
-            }
-
-            return bestPerYear;
-        }
-    }
-};
-
 const about = {
     template: `
         <div>
@@ -71,19 +17,15 @@ const about = {
         </div>`
 };
 
-const routes = [
-    {path: '/best-per-year', component: bestPerYear},
-    {path: '/about', component: about},
-];
-
-const router = new VueRouter({
-    routes: routes
-});
-
 const app = new Vue({
     el: '#app',
 
-    router: router,
+    router: new VueRouter({
+        routes: [
+            {path: '/best-per-year', component: bestPerYear},
+            {path: '/about', component: about}
+        ],
+    }),
 
     data: {
         sharedState: store.state,
